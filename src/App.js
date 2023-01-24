7425b0f2eabb967be28e0ead8b6ec64ad7c047d9
Zzzzzshan <zshanfarooq67@gmail.com>
2023-01-25 00:45:19 +0400

    Header work going on

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
-  return (
-   
-  );
+  return <Header />;
 }
 
 export default App;
